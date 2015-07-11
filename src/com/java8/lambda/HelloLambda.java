730452a5c61b6a445fe797eab339520184a8ed90
Liu Ronggen <ronggenliu@gmail.com>
2015-07-11 19:40:10 +0800

    learn lambda.

---
+++ src/com/java8/lambda/HelloLambda.java
@@ -0,0 +1,14 @@
+package com.java8.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloLambda {
+
+	private String str = "";
+	public static void main(String[] args) {
+		System.out.println("Hello, Sublime Text!");
+		List<String> list = new ArrayList<String>();
+		System.out.println("" + list.size());
+	}
+}
